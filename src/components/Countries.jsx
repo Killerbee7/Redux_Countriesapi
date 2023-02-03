f22e5fc2924deec3654a5abbf8ca802c7afb8f88
Dibya Dahal <102875464+Killerbee7@users.noreply.github.com>
2023-02-03 14:52:04 +0200

    landing page done

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -9,28 +9,26 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import Row from 'react-bootstrap/Row';
 import { LinkContainer } from 'react-router-bootstrap';
 import {initializeCountries} from "../features/countriesSlice";
+import CardImg from 'react-bootstrap/esm/CardImg';
+import Spinner from 'react-bootstrap/Spinner';
+
+
 
 
 const Countries = () => {
   const dispatch = useDispatch();
 
   const countriesList = useSelector((state) => state.countries.countries)
-
+  const loading = useSelector((state) => state.countries.isLoading)
   const [search, setSearch] = useState('')
 
-  //console.log("Search: ", search)
+
 
   useEffect(() => {
     dispatch(initializeCountries())
   },[dispatch])
 
-  // We will be replacing this with data from our API.
-  const country = {
-    name: {
-      common: 'Example Country'
-    }
-  }
-
+  
 
   return (
     <Container fluid>
@@ -48,37 +46,61 @@ const Countries = () => {
           </Form>
         </Col>
       </Row>
+      
       <Row xs={2} md={3} lg={4} className="g-3">
-            <Col className="mt-5">
-              <LinkContainer
-                to={`/countries/${country.name.common}`}
-                state={{ country: country }}
-              >
-                <Card className="h-100">
-                  <Card.Body className="d-flex flex-column">
-                    <Card.Title>{'Single Country Common Name'}</Card.Title>
-                    <Card.Subtitle className="mb-5 text-muted">
-                      {'Single Country Official Name'}
-                    </Card.Subtitle>
-                    <ListGroup
-                      variant="flush"
-                      className="flex-grow-1 justify-content-end"
-                    >
-                      <ListGroup.Item>
-                        <i className="bi bi-translate me-2"></i>
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        <i className="bi bi-cash-coin me-2"></i>
-                      </ListGroup.Item>
+        {(loading) ? <Spinner animation="border" variant="primary"  /> : ''}
+        
+      
+        {countriesList.filter((c) => {
+          return c.name.official
+          .toLowerCase()
+          .includes(search.toLowerCase());
+        }).map((country)=>(  
+          
+          <Col className="mt-5">
+          <LinkContainer
+            to={`/countries/${country.name.common}`}
+            state={{ country: country }}
+          >
+            
+            <Card className="h-100">
+              <Card.Body className="d-flex flex-column">
 
-                      <ListGroup.Item>
-                        <i className="bi bi-people me-2"></i>
-                      </ListGroup.Item>
-                    </ListGroup>
-                  </Card.Body>
-                </Card>
-              </LinkContainer>
-            </Col>
+                <CardImg src={country.flags.svg}></CardImg>
+            
+                <Card.Title>{country.name.common}</Card.Title>
+                <Card.Subtitle className="mb-5 text-muted">
+                  {'Single Country Official Name'}
+                </Card.Subtitle>
+                <ListGroup
+                  variant="flush"
+                  className="flex-grow-1 justify-content-end"
+                >
+                  <ListGroup.Item>
+                    
+                    <i className="bi bi-translate me-2"></i>
+                    {Object.values(country.languages?? {}).join(',')}
+                    {/* {country.languages ? Object.values(country.languages).join(", ")
+                    : "__"} */}
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                  
+                    <i className="bi bi-cash-coin me-2"></i>
+                    {country.currencies ? Object.values(country.currencies).map((currency) =>currency.name).join(","): "__"}
+                    
+                  </ListGroup.Item>
+
+                  <ListGroup.Item>
+                    <i className="bi bi-people me-2"></i>
+                    {(country.population).toLocaleString('en-US')}
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card.Body>
+            </Card>
+          </LinkContainer>
+        </Col> 
+        ))}
+            
       </Row>
     </Container>
   );
--- src/features/countriesSlice.js
+++ src/features/countriesSlice.js
@@ -1,15 +1,21 @@
 import {createSlice} from "@reduxjs/toolkit"
 import countryService from "../services/countries"
 
+
 export const countrySlice = createSlice({
     name: 'countries',
     initialState:{
         countries:[],
+        isLoading: true,
     },
 
     reducers:{
         getCountries(state, action){
             state.countries= action.payload
+        },
+
+        isLoading(state, action){
+            state.isLoading = action.payload
         }
     },
 })
@@ -18,9 +24,11 @@ export const initializeCountries = () => {
     return async (dispatch) => {
         const countries = await countryService.getAll();
         dispatch(getCountries(countries));
+        dispatch(isLoading(false))
     }
 }
 
-export const {getCountries} = countrySlice.actions
+export const {getCountries, isLoading} = countrySlice.actions
 
 export default countrySlice.reducer
+
