b75eec9d8af6332d1fedddfd04a26a447c25f324
Dibya Dahal <102875464+Killerbee7@users.noreply.github.com>
2023-04-11 23:40:49 +0300

    layout css changes

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -17,7 +17,7 @@ const Layout = () => {
   return (
     <Container fluid>
       <Row>
-        <Navbar bg="light" variant="light">
+        <Navbar bg="dark" variant="light">
           <Container className="justify-content-end">
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
